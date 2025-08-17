11bc7b259ee53bd8e3f86d958bda2c2bb7e50aae
karpaguy <denuzzidouglas@gmail.com>
2025-08-17 10:26:26 -0300

    Finalized

--- JSYuGiOh/src/scripts/engine.js
+++ JSYuGiOh/src/scripts/engine.js
@@ -189,6 +189,9 @@ function init() {
     
     drawCards(5, state.playerSides.player1)
     drawCards(5, state.playerSides.computer)
+
+    const bgm = document.getElementById("bgm");
+    bgm.play()
 }
 
 init()
